2cf6b3211ef1ba592cbe451a5a3ba59522261b40
Ic3 <chitrathep.c@codium.co>
2020-08-19 14:24:20 +0700

    Run Tested

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
 // Your implementations
 // 
 
-export const updateGameScore = (player1Points, player2Points) => {
+export const updateGameScore = (player1Points: Number, player2Points: Number) => {
     if (player1Points > player2Points) {
         return 1;
     }
@@ -10,14 +10,14 @@ export const updateGameScore = (player1Points, player2Points) => {
         return 2;
 }
 
-export const decideGameWinner = (game) => {
+export const decideGameWinner = (game: Number) => {
     if (game === 1)
         return 'Player 1'
     else
         return 'Player 2'
 }
 
-export const printScore = (playerPoints) => {
+export const printScore = (playerPoints: Array<number>) => {
     if (playerPoints[0] === playerPoints[1] && playerPoints[1] !== 3)
         return 'thirty - thirty';
     if (playerPoints[0] === playerPoints[1])
